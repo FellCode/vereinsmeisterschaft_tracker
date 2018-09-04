abf9c468995afda93d59c6721ef09049a43ad889
Felix Smuda <f.smudo@googlemail.com>
2018-09-04 06:40:37 +0000

    add middleware to result route

--- routes/result.js
+++ routes/result.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var Result = require('../models/result');
+var middleware = require("../middleware");
 
 
-router.get('/:id/edit', function(req, res) {
+router.get('/:id/edit',middleware.isLoggedIn, function(req, res) {
 	Result.findById(req.params.id,function(err,foundResult){
 		if(err){
 			// eslint-disable-next-line no-console 
@@ -15,7 +16,7 @@ router.get('/:id/edit', function(req, res) {
 });
 
 
-router.put('/:id', function(req, res) {
+router.put('/:id',middleware.isLoggedIn, function(req, res) {
 	Result.findByIdAndUpdate(req.params.id,req.body.result,function(err){
 		if(err){
 			// eslint-disable-next-line no-console 
