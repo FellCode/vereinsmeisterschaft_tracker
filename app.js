1a1742849cf80b93d97fa5c7c8d439315656f071
Felix Smuda <f.smudo@googlemail.com>
2018-09-04 06:27:44 +0000

    add authentication

--- app.js
+++ app.js
@@ -4,15 +4,18 @@ var app = express();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
 var dbUtils = require ('./dbUtils.js');
+
+//MODELS
+var User = require('./models/user');
 
 //ROUTES
 var indexRoutes = require('./routes/index');
 var teilnehmerRoutes = require('./routes/teilnehmer');
 var meisterschaftRoutes = require('./routes/meisterschaft');
 var resultRoutes = require('./routes/result');
-//UTILS
-
 
 
 //DATABASE CONNECTION
@@ -24,6 +27,22 @@ app.set('view engine', 'ejs');
 // eslint-disable-next-line no-undef
 app.use(express.static(__dirname + '/public'));
 app.use(methodOverride('_method'));
+app.use(require("express-session")({
+    secret: 'what is love baby dont hurt me',
+    resave:false,
+    saveUninitialized:false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req,res,next){
+    res.locals.currentUser = req.user;
+    next();
+});
 
 
 //ROUTES
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,47 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+
+var User = require('../models/user');
 
 //ROOT
 router.get('/',function(req,res){
-	res.render('landing');
+	res.render('login');
 });
+
+
+//AUTH
+router.get("/register", function(req, res) {
+    res.render("register");
+});
+router.post("/register", function(req, res) {
+    var newUser = new User({username: req.body.username});
+    User.register(newUser,req.body.password, function(err,user){
+        if(err){
+            return res.render("register");
+        } else{
+            passport.authenticate("local")(req,res, function(){
+                res.redirect("/meisterschaft");
+            });
+        }
+    });
+});
+router.get("/login", function(req, res) {
+    res.render("login");
+});
+router.post("/login",passport.authenticate("local",
+    {
+        successRedirect: "/meisterschaft",
+        failureRedirect: "/login"
+    }), function(req, res) {
+        
+});
+router.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/login");
+});
+
+
+
 
 module.exports = router;
