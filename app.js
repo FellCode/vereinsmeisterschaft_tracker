ca96b616c5a4d05565a2d116721068253367ddf0
Felix Smuda <f.smudo@googlemail.com>
2019-07-08 16:27:54 +0000

    url fix

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var resultRoutes = require('./routes/result');
 
 mongoose.set('useFindAndModify', false);
 
-mongoose.connect(process.env.DB_URL);
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-6qd5o.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,{ useNewUrlParser: true });
 
 
 //SETTINGS
